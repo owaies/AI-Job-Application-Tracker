ce0152b5ea41edfded6eda9f432a20e8a86c437f
owaies <owaies786@gmail.com>
2026-09-02 20:59:45 +0530

    fix: load mobile layout safety styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './brutalist-fixes.css'
+import './mobile-safety.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
