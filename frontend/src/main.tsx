5f0f6213a9070f22cda579a0c140299932354157
owaies <owaies786@gmail.com>
2026-09-02 21:17:28 +0530

    fix: protect production UI from render crashes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ErrorBoundary } from './ErrorBoundary'
 import './styles.css'
 import './brutalist-fixes.css'
 import './mobile-safety.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
